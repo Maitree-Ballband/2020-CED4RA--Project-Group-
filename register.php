8242ea30959ef11b136426e6fff5eb42c13c8a86
KhunKidta <48791730+KhunKidta@users.noreply.github.com>
2020-08-01 19:09:08 +0700

    project01

---
+++ register.php
@@ -0,0 +1,90 @@
+<?php 
+
+    session_start();
+
+    require_once "connection.php";
+
+    if (isset($_POST['submit'])) {
+
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        $firstname = $_POST['firstname'];
+        $lastname = $_POST['lastname'];
+
+        $user_check = "SELECT * FROM user WHERE username = '$username' LIMIT 1";
+        $result = mysqli_query($conn, $user_check);
+        $user = mysqli_fetch_assoc($result);
+
+        if ($user['username'] === $username) {
+            echo "<script>alert('Username already exists');</script>";
+        } else {
+            $passwordenc = md5($password);
+
+            $query = "INSERT INTO user (username, password, firstname, lastname, userlevel)
+                        VALUE ('$username', '$passwordenc', '$firstname', '$lastname', 'm')";
+            $result = mysqli_query($conn, $query);
+
+            if ($result) {
+                $_SESSION['success'] = "Insert user successfully";
+                header("Location: index.php");
+            } else {
+                $_SESSION['error'] = "Something went wrong";
+                header("Location: index.php");
+            }
+        }
+
+    }
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Register Page</title>
+     <!-- Required meta tags -->
+     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+
+</head>
+<body>
+
+
+<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+  <div class="form-group">
+    <label for="username">username:</label>
+    <input type="text" class="form-control" placeholder="Enter username" id="username"required>
+  </div>
+  <div class="form-group">
+    <label for="pwd">Password:</label>
+    <input type="password" class="form-control" placeholder="Enter password" id="password"required>
+  </div>
+  <div class="form-group">
+    <label for="pwd">Firstname:</label>
+    <input type="Firstname" class="form-control" placeholder="Enter Firstname" id="Firstname"required>
+  </div>
+  <div class="form-group">
+    <label for="pwd">Lastname:</label>
+    <input type="Lastname" class="form-control" placeholder="Enter Lastname" id="Lastname"required>
+  </div>
+  <button type="submit" class="btn btn-primary">Login</button>
+  
+</form>
+
+    <a href="index.php">Go back to index</a>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+</body>
+</html>
